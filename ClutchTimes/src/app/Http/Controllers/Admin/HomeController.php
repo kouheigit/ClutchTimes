2c50975c4cd33f472dad2c310ed8a94afbc8c27e
USER <user@USERnoMacBook-Air.local>
2022-06-07 14:40:45 +0900

    reserved

--- ClutchTimes/src/app/Http/Controllers/Admin/HomeController.php
+++ ClutchTimes/src/app/Http/Controllers/Admin/HomeController.php
@@ -33,7 +33,24 @@ class HomeController extends Controller
     }
     public function  addnewspost(Request $request)
     {
-        $msg = $request->input('msg');
-        return view('admin.addnews',compact('msg'));
+        //値を取得
+        $title = $request->input('title');
+        $date1 = $request->input('date');
+        $time = $request->input('time');
+        $comment = $request->input('comment');
+
+        //$todayはテスト項目
+        $today = date("Y-m-d H:i:s");
+        //取得した値をY-m-d型に変換
+        $date2 = $date1 .$time;
+        $date = date('Y-m-d H:i:s' ,strtotime($date2));
+
+        //テストコード$date(予定の日時)が$today(現在の日時)に至ってない。
+        //ニュース画面にこの処理を入れる
+        if($date > $today){
+            $date ="まだ投稿予定の時刻じゃありません";
+        }
+
+        return view('admin.addnews',compact('title','date','comment','today'));
     }
 }
--- ClutchTimes/src/resources/views/admin/addnews.blade.php
+++ ClutchTimes/src/resources/views/admin/addnews.blade.php
@@ -1,13 +1,38 @@
 @extends('layouts.app')
-
 @section('content')
-   @isset($msg)
-       <p>{{$msg}}</p>
-   @else
-   @endisset
+    <!--テスト表示させる-->
+    @isset($title)
+        <p>{{$title}}</p>
+    @else
+    @endisset
+    @isset($comment)
+        <p>{{$comment}}</p>
+    @else
+    @endisset
+    @isset($date)
+        <p>{{$date}}</p>
+    @else
+    @endisset
+    @isset($today)
+        <p>{{$today}}</p>
+    @else
+    @endisset
+    <!--テスト表示終了-->
+
     <form method="POST" action="{{ route('admin.addnews') }}">
-    　@csrf
-       <input type="text" name="msg">
-       <input type="submit">
+        　@csrf
+        <!--タイトル-->
+        <p>タイトル</p>
+        <input type="text" name="title" required>
+        <!--投稿日時-->
+        <p>投稿日時</p>
+        <label for="date">投稿時刻</label>
+        <input type="date" id="date" name="date" value="" required/>
+        <input type="time" id="time" name="time" value="" required/>
+        <!--本文-->
+        <p>本文</p>
+        <textarea name="comment" cols="40" rows="8" required></textarea>
+        <br>
+        <input type="submit" value="投稿する">
     </form>
 @endsection
